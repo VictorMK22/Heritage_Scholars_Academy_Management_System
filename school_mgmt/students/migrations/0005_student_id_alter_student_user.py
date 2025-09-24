6a9ead5d88f8ec4877800057fb70c07dcf8cf78f
VictorMK22 <meli.victorkip17@gmail.com>
2025-09-24 18:08:04 +0300

    Fix migration 0005: add id field without primary key constraint

--- school_mgmt/students/migrations/0005_student_id_alter_student_user.py
+++ school_mgmt/students/migrations/0005_student_id_alter_student_user.py
@@ -1,18 +1,21 @@
+# 0005_student_id_alter_student_user.py
 import django.db.models.deletion
 from django.conf import settings
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
-
     dependencies = [
         ('students', '0004_student_roll_no'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
-        # Remove the AddField operation that tries to add a second primary key
-        # Only keep the AlterField operation
+        migrations.AddField(
+            model_name='student',
+            name='id',
+            field=models.AutoField(auto_created=True, default=1, serialize=False),  # No primary_key=True!
+            preserve_default=False,
+        ),
         migrations.AlterField(
             model_name='student',
             name='user',
