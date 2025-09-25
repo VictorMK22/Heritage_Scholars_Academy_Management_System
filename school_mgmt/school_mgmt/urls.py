0aa5a63c987fa464caad3f0a0667e87a26519506
VictorMK22 <meli.victorkip17@gmail.com>
2025-09-25 16:41:29 +0300

    Modified the root app urls file

--- school_mgmt/school_mgmt/urls.py
+++ school_mgmt/school_mgmt/urls.py
@@ -35,6 +35,5 @@ urlpatterns = [
     path('privacy-policy/', TemplateView.as_view(template_name="school_mgmt/privacy_policy.html"), name='privacy_policy')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
